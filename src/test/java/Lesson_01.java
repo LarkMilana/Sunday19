8550f2d8bb8e76ff83cea668f8e52e5238e1f9cd
LarkMilana <larkmilana@gmail.com>
2023-03-19 22:23:01 +0200

    Test Start Lesson_01

---
+++ src/test/java/Lesson_01.java
@@ -0,0 +1,8 @@
+public class Lesson_01 {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello World!");
+        System.out.printf("Hello World! 02");
+        System.out.printf("Hello World! 03");
+    }
+}
